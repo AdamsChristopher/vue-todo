cc0fa7ca414e13d74f92b528b271213e6631644c
AdamsChristopher <neanderthalnoise@gmail>
2019-04-23 09:51:47 +0200

    First commit

---
+++ src/components/AddTodo.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <form v-on:submit="addTodo">
+      <input 
+        type="text" 
+        name="title" 
+        placeholder="Add Todo..."
+        v-model="title"
+      >
+      <input 
+        class="button" 
+        type="submit" 
+        value="Sublit"
+      >
+    </form>
+  </div>
+</template>
+
+<script>
+// import uuid from 'uuid';
+
+export default {
+  name: "AddTodo",
+  data() {
+    return {
+      title: ''
+    }
+  },
+  methods: {
+    addTodo(event) {
+      event.preventDefault();
+      const newTodo = {
+        // id: uuid.v4(),
+        title: this.title,
+        completed: false
+      }
+      this.$emit('add-todo', newTodo);
+      this.title = '';
+    }
+  }
+}
+</script>
+
+<style scoped>
+form {
+  display: flex;
+}
+input[type="text"] {
+  flex: 10;
+  padding: 5px;
+}
+input[type="submit"] {
+  flex: 2;
+}
+</style>
---
+++ src/views/About.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="about">
+    <h1>About</h1>
+    <p>This is the TodoList app v1.0.0</p>
+  </div>
+</template>
+
+<style scoped>
+  .about {
+    text-align: center;
+    margin-top: 10px;
+  }
+</style>
+
